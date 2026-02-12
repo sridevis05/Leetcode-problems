6b5df4f3eb860a66a7401c9b0128e6e37da12693
sridevis05 <mails.sridevi@gmail.com>
2026-02-12 09:54:04 +0530

    find

---
+++ lc448.py
@@ -0,0 +1,10 @@
+from typing import List 
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+           n=len(nums)
+           nums=set(nums)
+           res=[]
+           for i in range(1,n+1):
+               if i not in nums:
+                    res.append(i)
+           return res
