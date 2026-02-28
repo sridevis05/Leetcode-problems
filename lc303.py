96f0ee5bd1ac9781fc4a410e6c2e35b8b4b2b12c
sridevis05 <mails.sridevi@gmail.com>
2026-02-28 12:36:21 +0530

    total

--- lc303.py
+++
@@ -1,8 +0,0 @@
-class NumArray:
-    def __init__(self, nums):
-        self.nums=nums
-    def sumRange(self,left,right):
-        total=0
-        for i in range(left,right+1):
-            total+=self.nums[i]
-        return total
